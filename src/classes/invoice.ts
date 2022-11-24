27c6e10e7b62441451497368f83316da5f671c56
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-11-24 06:40:56 +0100

    Update invoice.ts

--- src/classes/invoice.ts
+++ src/classes/invoice.ts
@@ -5,6 +5,7 @@ export class Invoice{
       this.details=details;
       this.amount=amount;
     }
+
     format(){
      // return this.name + " owes " + this.amount + " for " + this.details;  or
      return `${this.name} owes #${this.amount} for ${this.details}`;
